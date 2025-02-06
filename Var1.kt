a5efee3a4c8580a16308070de8f70e70fe4cd5ae
arjun98k <arjunkandekar14@gmail.com>
2025-02-06 10:46:56 +0530

    variable in kt

--- Var1.kt
+++ Var1.kt
@@ -5,5 +5,10 @@ fun main() {
     println(ak)
     // val is cannot change it value it remain same you cant change it
     val am = 98
-    print(am)
+//    print(am)
+
+    ak = 13
+    println("it allow to change value $ak")
+
+
 }
