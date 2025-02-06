d0f88c9ba90c115cb4cbb19d712d1f1831d40166
arjun98k <arjunkandekar14@gmail.com>
2025-02-06 11:09:36 +0530

    types of data in kotlin

---
+++ KotinDatatype.kt
@@ -0,0 +1,20 @@
+// data type in kotlin
+// 1) Number 2) Character 3) Boolean 4) Array 5) String
+
+// Boolean Represent true or false
+
+fun main() {
+//    val flags = true
+//    println(flags)
+
+    var arr = arrayOf(1,3,4,5,7)
+ var firstId = arr[0]
+//    println(firstId)
+    for (i in arr){
+       if (i == 4){
+           println("element found $i")
+       }
+    }
+
+
+}
