38ab9fea3e9c15c64a0ca37b0c7e5f68239ac0b5
arjun98k <arjunkandekar14@gmail.com>
2025-02-06 12:04:16 +0530

    Operator in kotlin max num prog

---
+++ OperatorKotlin.kt
@@ -0,0 +1,12 @@
+import java.util.Scanner
+fun main() {
+    println("enter the value of a")
+    var a: Int =Integer.valueOf(readLine())
+    println("enter the value of b")
+    var b: Int =Integer.valueOf(readLine())
+    if (a > b){
+        println("a is greater than b max $a")
+    }else{
+        println("b is greater than  a max $b")
+    }
+}
