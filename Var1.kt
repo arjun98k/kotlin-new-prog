75976298046579f4e0ca3740b08065eef3842900
arjun98k <arjunkandekar14@gmail.com>
2025-02-06 10:42:56 +0530

    variable in kt

---
+++ Var1.kt
@@ -0,0 +1,9 @@
+fun main() {
+
+    // var is mutable and can change variable
+    var ak = 11
+    println(ak)
+    // val is cannot change it value it remain same you cant change it
+    val am = 98
+    print(am)
+}
