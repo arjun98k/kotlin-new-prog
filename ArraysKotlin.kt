074556167f33faf2de663e01fccea411725247ae
arjun98k <arjunkandekar14@gmail.com>
2025-02-07 00:14:10 +0530

    Arrays of kotlin

--- ArraysKotlin.kt
+++ ArraysKotlin.kt
@@ -1,6 +1,10 @@
 fun main() {
     var arr = arrayOf<String>("anamika", "aditi","anushka","ayushi")
+//    for (i in 0..3){
+//        println(arr[i])
+//    }
+    arr.set(0,"Arjun")
     for (i in 0..3){
-        println(arr[i])
-    }
+       println(arr[i])
+  }
 }
