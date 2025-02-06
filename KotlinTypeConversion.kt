2f44f94cb9960d89c4f5ab3d4d0de74d5a7dd2ce
arjun98k <arjunkandekar14@gmail.com>
2025-02-06 12:04:52 +0530

    type of conversation

--- KotlinTypeConversion.kt
+++ KotlinTypeConversion.kt
@@ -2,5 +2,14 @@ fun main() {
     var val1 = 98
 //    val value2: Long = value1  //Compile error, type mismatch
     val val2: Long = val1.toLong()
+//    The list of helper functions used for numeric conversion in Kotlin is given below:
+//
+//    toByte()
+//    toShort()
+//    toInt()
+//    toLong()
+//    toFloat()
+//    toDouble()
+//    toChar()
 
 }
