fe36fa02d751f13fdadd0acaa6db762d3c727fff
arjun98k <arjunkandekar14@gmail.com>
2025-02-06 14:05:46 +0530

    ForLoops

---
+++ ForLoops1.kt
@@ -0,0 +1,15 @@
+fun main() {
+//    for (i in 1..5){
+//        println(i)
+//    }
+//    for (i in 5 downTo 1)
+//        println(i)
+
+//    for (i in 1..6 step 2)
+//        println(i)
+
+    for (i in 10 downTo 0 step 2)
+        println(i)
+
+
+}
