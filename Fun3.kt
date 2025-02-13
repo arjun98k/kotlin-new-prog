48c5f60ce7203c976c7764deaa4fba2d6a112993
arjun98k <arjunkandekar14@gmail.com>
2025-02-14 01:29:59 +0530

    fun with default agrument

--- Fun3.kt
+++ Fun3.kt
@@ -1,9 +1,15 @@
 fun main() {
-    oddAndEven(11)
+//    oddAndEven(11)
+//    love(5)
+    love() // default argyment
 }
 
 fun oddAndEven(num1:Int):Unit = if (num1 %2 == 0){
     println("$num1 it is even number")
 }else{
     println("it is odd number")
+}
+
+fun love(num1:Int =3){
+    for (a in 1..num1 ) println("happy valentine $a")
 }
