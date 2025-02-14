b9046f7795f8064e7c109964acdf536afd796151
arjun98k <arjunkandekar14@gmail.com>
2025-02-14 21:48:11 +0530

    getter and setter

--- oOPS/OOPS4.kt
+++ oOPS/OOPS4.kt
@@ -1,14 +1,24 @@
 package oOPS
 
 fun main() {
-val ak = Person1("arjun", 14)
+var ak = Person1("arjun", 50)
     println(ak.name)
+  ak.age = 40
+    println(ak.age)
 }
 
-class  Person1(nameparam:String, age:Int){
-    var name:String = nameparam
-        get(){
+class  Person1(nameparam:String, ageParam:Int) {
+    var name: String = nameparam
+        get() {
             return field.uppercase()
         }
+    var age: Int = ageParam
+        set(value) {
+            if (value > 0) {
+                field = value
+            } else {
+                println("value is negative")
+            }
 
+        }
 }
