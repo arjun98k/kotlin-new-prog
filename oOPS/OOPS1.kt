7e5c3f31b603b29394618b3b10cc05c6b94c8b61
arjun98k <arjunkandekar14@gmail.com>
2025-02-14 13:59:01 +0530

    OOPS in kt

--- oOPS/OOPS1.kt
+++ oOPS/OOPS1.kt
@@ -1,5 +1,18 @@
 package oOPS
 
 fun main() {
+val obj = Car("maruti", 60, 25)
+    val obj2= Car("nexa", 65, 21)
+    obj.driveCar()
+    obj.infoCar()
+    obj2.infoCar()
+}
 
+class  Car(val name:String, val speed:Int, var range:Int){
+    fun driveCar(){
+        println("car name is $name")
+    }
+    fun infoCar(){
+        println("speed of car is $speed and range is $range")
+    }
 }
