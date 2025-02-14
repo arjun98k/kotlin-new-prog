43564ddb223f8a1d96394b6f0c8588b5978d5064
arjun98k <arjunkandekar14@gmail.com>
2025-02-14 13:26:01 +0530

    arrays in kotlin

---
+++ arrayskot.kt
@@ -0,0 +1,12 @@
+fun main() {
+    var arr = arrayOf("aksh","arjun","aditya","amit","adwait")
+//    for((i,z) in arr.withIndex()){
+//        println("$i and index are $z")
+//
+//    }
+    println( arr.get(3))
+   arr.set(4,"aahan")
+    println(arr.get(4))
+    println(arr.size)
+
+}
