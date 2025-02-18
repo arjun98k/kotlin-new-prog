cfd175a550d610acfc6f4a2188783463b1c6f0fb
arjun98k <arjunkandekar14@gmail.com>
2025-02-18 21:03:34 +0530

    list of collection

--- collection/col1.kt
+++ collection/col1.kt
@@ -1,6 +1,7 @@
 package collection
 
 fun main() {
+    //immutable list in collection
     var listi1 = listOf< String>("adi", "aksh","arjun")
 
 //    for (index in 0..listi1.size-1){
@@ -10,7 +11,13 @@ fun main() {
     var list2 = mutableListOf< String>( "amit","adwait","ajay" )
     list2.add(3,"atul")
 
-    for (index in 0..list2.size-1){
-        println("list2 element $index "+ list2[index])
+//    for (index in 0..list2.size-1){
+//        println("list2 element $index "+ list2[index])
+//    }
+
+    var list3 = arrayListOf<String>("amita","aditi")
+    list3.add(2,"swara")
+        for (index in 0..list3.size-1){
+        println("list3 element $index "+ list3[index])
     }
 }
