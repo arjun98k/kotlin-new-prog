e2b921ce1099677a9b8e7273b6522631f123c4f0
arjun98k <arjunkandekar14@gmail.com>
2025-02-18 21:21:20 +0530

    list of collection of map and hashmap

--- collection/col2.kt
+++ collection/col2.kt
@@ -2,8 +2,16 @@ package collection
 
 fun main() {
     var mymap = mapOf<Int,String>(11 to "aksh",12 to "amita", 13 to "aditi")
-    for (key in mymap.keys){
-        println(mymap[key])
+//    for (key in mymap.keys){
+//        println(mymap[key])
+//    }
+    var mymap2 = HashMap<Int, String>()
+    mymap2.put(1,"aditya")
+    mymap2.put(2,"amita")
+    mymap2.put(3,"atul")
+
+    for (key in mymap2.keys){
+        println("the index of mymap are  and the values are $key = ${mymap2[key]}")
     }
 
 }
