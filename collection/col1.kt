09431b14eec950878b785c6e3c60ee8e579103e2
arjun98k <arjunkandekar14@gmail.com>
2025-02-18 20:56:23 +0530

    list of collection

--- collection/col1.kt
+++ collection/col1.kt
@@ -3,7 +3,14 @@ package collection
 fun main() {
     var listi1 = listOf< String>("adi", "aksh","arjun")
 
-    for (index in 0..listi1.size-1){
-        println("list of element $index " + listi1[index])
+//    for (index in 0..listi1.size-1){
+//        println("list of element $index " + listi1[index])
+//    }
+
+    var list2 = mutableListOf< String>( "amit","adwait","ajay" )
+    list2.add(3,"atul")
+
+    for (index in 0..list2.size-1){
+        println("list2 element $index "+ list2[index])
     }
 }
