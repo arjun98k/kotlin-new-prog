f6f14fad02bd6b0c762ac2088f246ddc80ade89c
arjun98k <arjunkandekar14@gmail.com>
2025-02-19 14:05:06 +0530

    filter in collection

--- collection/col4.kt
+++ collection/col4.kt
@@ -3,7 +3,8 @@ package collection
 fun main() {
     val myNum: List<Int> = listOf(11,33,5,65,30)
 
-    val mySmallNum = myNum.filter { it < 50 }.map { it * it } // it * it == {num -> num *num}
+//    val mySmallNum = myNum.filter { it < 50 }.map { it * it } // it * it == {num -> num *num}
+    val mySmallNum = myNum.filter{myNum-> if (myNum%2 == 0) { println("num is even"); true} else {println("num is odd");  false} }
    for (a in mySmallNum){
    println( (a))
 
