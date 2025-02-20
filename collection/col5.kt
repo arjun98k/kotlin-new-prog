f249300c1c267915496f52fa2e15925e277c2677
arjun98k <arjunkandekar14@gmail.com>
2025-02-20 23:52:17 +0530

    filter in collection

--- collection/col5.kt
+++ collection/col5.kt
@@ -4,6 +4,6 @@ fun main() {
     val num = listOf(11,12,13,14,15,16)
     val oddnum = num.filter { it %2 != 0 }
     val evenum = num.filter { it %2 == 0 }
-    println(oddnum)
-    println(evenum)
+    println("print odd number from list $oddnum")
+    println("print even number from list $evenum")
 }
