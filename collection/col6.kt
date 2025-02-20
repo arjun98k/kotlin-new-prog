f3e68347ed36cc313299306404d8650087875749
arjun98k <arjunkandekar14@gmail.com>
2025-02-20 23:55:40 +0530

    filter in collection

--- collection/col6.kt
+++ collection/col6.kt
@@ -4,4 +4,7 @@ fun main() {
     val num = listOf(1,2,3,4,5,6,7,8)
     val haseven = num.any{ it % 2 == 0 }
     println(haseven)
+    val haseven1 = num.all{ it % 2 == 0 }
+    println(haseven1)
+
 }
