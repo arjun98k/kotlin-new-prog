b4eed4afac141b5bfbca715f71db0f956ecd122c
arjun98k <arjunkandekar14@gmail.com>
2025-02-20 23:50:44 +0530

    filter in collection

--- collection/col5.kt
+++ collection/col5.kt
@@ -2,6 +2,8 @@ package collection
 
 fun main() {
     val num = listOf(11,12,13,14,15,16)
-    val evennum = num.filter { it %2 == 0 }
-    println(evennum)
+    val oddnum = num.filter { it %2 != 0 }
+    val evenum = num.filter { it %2 == 0 }
+    println(oddnum)
+    println(evenum)
 }
