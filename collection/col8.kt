33a933a6e270f25c4c591d280b18bbc207511386
arjun98k <arjunkandekar14@gmail.com>
2025-02-22 23:49:19 +0530

    practice lambda

--- collection/col8.kt
+++ collection/col8.kt
@@ -2,4 +2,11 @@ package collection
 fun main() {
     val ak:()-> Unit = {val a=14; val b=15; println(a+b)}
     ak()
+    val bk:() -> Unit ={
+        val age = readLine()!!.toInt()
+        if (age  >= 18 ){
+            print("he can drive")
+        }
+    }
+    bk()
 }
