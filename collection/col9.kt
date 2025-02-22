9c92ec2f6b8eb99be90090b1a5ef924ce3ef8af2
arjun98k <arjunkandekar14@gmail.com>
2025-02-22 23:58:41 +0530

    practice lambda

--- collection/col9.kt
+++ collection/col9.kt
@@ -2,7 +2,14 @@ package collection
 fun String.buildMessage(): String{
     return this + "is awesome"
 }
+
+fun makeAdd(x: Int): (Int) -> Int {
+    return {y -> x+y}
+}
 fun main() {
    val ans = "Arjun".buildMessage()
-    println(ans)
+//    println(ans)
+    val addsix = makeAdd(6)
+    println(addsix(5))
+
 }
